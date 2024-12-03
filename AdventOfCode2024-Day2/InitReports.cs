448ed8769d5ce028fc35f6d3456497beb9c6e9fa
gusemil <gusemil@hotmail.com>
2024-12-03 10:46:08 +0200

    Even more concise day 2

--- AdventOfCode2024-Day2/InitReports.cs
+++ AdventOfCode2024-Day2/InitReports.cs
@@ -6,21 +6,15 @@ namespace AdventOfCode2024_Day2
     {
         public static List<List<int>> InitAndReturnReports()
         {
-            List<List<int>> listOfReports = new List<List<int>>();
-
-            List<int> report1 = [7, 6, 5, 4, 3];
-            List<int> report2 = [1, 2, 7, 8, 9];
-            List<int> report3 = [9, 7, 6, 2, 1];
-            List<int> report4 = [1, 3, 2, 4, 5];
-            List<int> report5 = [8, 6, 4, 4, 1];
-            List<int> report6 = [1, 3, 6, 7, 9];
-
-            listOfReports.Add(report1);
-            listOfReports.Add(report2);
-            listOfReports.Add(report3);
-            listOfReports.Add(report4);
-            listOfReports.Add(report5);
-            listOfReports.Add(report6);
+            List<List<int>> listOfReports =
+            [
+                [7, 6, 5, 4, 3],
+                [1, 2, 7, 8, 9],
+                [9, 7, 6, 2, 1],
+                [1, 3, 2, 4, 5],
+                [8, 6, 4, 4, 1],
+                [1, 3, 6, 7, 9],
+            ];
 
             return listOfReports;
         }
