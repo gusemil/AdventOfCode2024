f3d6d378055e2fb590fd9c5c1647c4c056619d60
gusemil <gusemil@hotmail.com>
2024-12-03 11:59:04 +0200

    More checks but something is still missing

--- AdventOfCode2024-Day2/InitReports.cs
+++ AdventOfCode2024-Day2/InitReports.cs
@@ -2,6 +2,20 @@
 {
     public static class InitReports
     {
+        public static List<List<int>> InitAndReturnExampleReports()
+        {
+            List<List<int>> listOfReports =
+            [
+                [7, 6, 4, 2, 1],
+                [1, 2, 7, 8, 9],
+                [9, 7, 6, 2, 1],
+                [1, 3, 2, 4, 5],
+                [8, 6, 4, 4, 1],
+                [1, 3, 6, 7, 9]
+            ];
+
+            return listOfReports;
+        }
         public static List<List<int>> InitAndReturnReports()
         {
             List<List<int>> listOfReports =
--- AdventOfCode2024-Day2/Program.cs
+++ AdventOfCode2024-Day2/Program.cs
@@ -4,55 +4,63 @@ Console.WriteLine("Advent of Code 2024 Day 2!");
 
 int safeReports = 0;
 
+//var reports = InitReports.InitAndReturnExampleReports();
 var reports = InitReports.InitAndReturnReports();
 
-for(int i=0; i<reports.Count; i++)
+for (int i=0; i<reports.Count; i++)
 {
-    safeReports += CheckLevelSafety(reports[i]);
+    safeReports += CheckLevelSafety(reports[i], true, true);
 }
 
 
 Console.WriteLine("Safe reports amount: " + safeReports);
 
 
-int CheckLevelSafety(List<int> inReport, bool writeLogs = false)
+int CheckLevelSafety(List<int> inReport, bool writeResult = false, bool writeLogs = false)
 {
-    int totalIncreasing = 0;
+    int lastResult = 0;
     for (int i = 0; i < inReport.Count - 1; i++)
     {
         int increasing = 0;
-        if (inReport[i] > inReport[i + 1])
+        if (inReport[i] < inReport[i + 1])
         {
+            if(i > 0 && lastResult != 1)
+            {
+                if (writeResult) Console.WriteLine("UNSAFE");
+                return 0;
+            }
             //Increasing
             increasing = 1;
+            if (writeLogs) Console.WriteLine(inReport[i] + " VS " + inReport[i +1] + " INCREASING");
         }
-        else if (inReport[i] < inReport[i + 1])
+        else if (inReport[i] > inReport[i + 1])
         {
+            if (i > 0 && lastResult != -1)
+            {
+                if (writeResult) Console.WriteLine("UNSAFE");
+                return 0;
+            }
             //Decreasing
             increasing = -1;
-        }
-
-        if(totalIncreasing == 0)
-        {
-            totalIncreasing = increasing;
+            if (writeLogs) Console.WriteLine(inReport[i] + " VS " + inReport[i + 1] + " DECREASING");
         }
         else
         {
-            if(totalIncreasing != increasing)
-            {
-                if(writeLogs) Console.WriteLine("UNSAFE");
-                return 0;
-            }
+            //Stale
+            if (writeResult) Console.WriteLine("STALE UNSAFE");
+            return 0;
         }
+
+        lastResult = increasing;
 
         int difference = Math.Abs(inReport[i] - inReport[i + 1]);
         if (difference > 3 || difference == 0)
         {
-            if (writeLogs) Console.WriteLine("UNSAFE");
+            if (writeResult) Console.WriteLine("UNSAFE");
             return 0;
         }
     }
 
-    if (writeLogs) Console.WriteLine("SAFE");
+    if (writeResult) Console.WriteLine("SAFE");
     return 1;
 }
